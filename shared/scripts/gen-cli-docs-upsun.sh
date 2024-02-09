82060acd05f8c09ead8f75b44f95d06e12679498
Florent Huck <flovntp@gmail.com>
2024-02-09 15:13:39 +0100

    changing path from upsun to friday

--- shared/scripts/gen-cli-docs-upsun.sh
+++ shared/scripts/gen-cli-docs-upsun.sh
@@ -3,11 +3,11 @@
 cp shared/pages/commands.md sites/friday/src/administration/cli/reference.md
 
 VERSION=$(upsun version)
-printf "\n# $VERSION\n" >> sites/upsun/src/administration/cli/reference.md
+printf "\n# $VERSION\n" >> sites/friday/src/administration/cli/reference.md
 
-printf "\n- [Installation](/administration/cli#1-install)" >> sites/upsun/src/administration/cli/reference.md
-printf "\n- [Open an issue](https://github.com/platformsh/cli/issues)\n" >> sites/upsun/src/administration/cli/reference.md
+printf "\n- [Installation](/administration/cli#1-install)" >> sites/friday/src/administration/cli/reference.md
+printf "\n- [Open an issue](https://github.com/platformsh/cli/issues)\n" >> sites/friday/src/administration/cli/reference.md
 
-printf "\n## All commands\n" >> sites/upsun/src/administration/cli/reference.md
+printf "\n## All commands\n" >> sites/friday/src/administration/cli/reference.md
 
-upsun list --format=md | tail -n +2 >> sites/upsun/src/administration/cli/reference.md
+upsun list --format=md | tail -n +2 >> sites/friday/src/administration/cli/reference.md
